73c5e7532f83b8ff7be260e9e158214db52cb358
Soonoh <soonoh.jung@gmail.com>
2024-08-29 14:38:11 +0900

    refactor: run prettier

--- src/md-to-notion-cli.ts
+++ src/md-to-notion-cli.ts
@@ -4,33 +4,40 @@ import { printFolderHierarchy, readMarkdownFiles, syncToNotion } from "./index"
 import { Command } from "commander"
 import { description, version } from "../package.json"
 
-const program = new Command();
+const program = new Command()
 
-async function main(directory: string, options: { verbose: boolean; token: string; pageId: string; }) {
-  const dir = readMarkdownFiles(directory);
+async function main(
+  directory: string,
+  options: { verbose: boolean; token: string; pageId: string }
+) {
+  const dir = readMarkdownFiles(directory)
 
   if (options.verbose) {
-    printFolderHierarchy(dir);
+    printFolderHierarchy(dir)
   }
 
   if (dir) {
-    await syncToNotion(
-      options.token,
-      options.pageId,
-      dir
-    );
+    await syncToNotion(options.token, options.pageId, dir)
   }
 
-  console.log("Sync complete!");
+  console.log("Sync complete!")
 }
 
 program
   .version(version)
   .description(description)
   .argument("<directory>", "Directory containing markdown files")
-  .option("-t, --token <token>", "Notion API Token, default is environment variable NOTION_API_TOKEN", process.env["NOTION_API_TOKEN"])
-  .option("-p, --page-id <id>", "Target Notion root page ID, default is env MD_TO_NOTION_PAGE_ID", process.env["MD_TO_NOTION_PAGE_ID"])
+  .option(
+    "-t, --token <token>",
+    "Notion API Token, default is environment variable NOTION_API_TOKEN",
+    process.env["NOTION_API_TOKEN"]
+  )
+  .option(
+    "-p, --page-id <id>",
+    "Target Notion root page ID, default is env MD_TO_NOTION_PAGE_ID",
+    process.env["MD_TO_NOTION_PAGE_ID"]
+  )
   .option("-v, --verbose", "Print folder hierarchy", false)
-  .action(main);
+  .action(main)
 
-program.parse(process.argv);
+program.parse(process.argv)
